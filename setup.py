746d34fd42ee528c3c5a6cd52e140b6f255d9964
Michael Browne <mikelcbrowne@gmail.com>
2019-06-29 18:49:48 -0600

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploring clinical trial news releases for factors that could potentially affect the prices of the company's corresponding stock prices.',
+    author='Michael Browne',
+    license='',
+)
