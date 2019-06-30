54c32637500d39bc859e73bc5e05c19e3787790c
Michael Browne <mikelcbrowne@gmail.com>
2019-06-29 22:15:42 -0600

    Working on 01-FirstDataExploration Notebook

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Exploring clinical trial news releases for factors that could potentially affect the prices of the company's corresponding stock prices.',
+    description='Exploring clinical trial news releases for factors that could potentially affect the prices of the companys corresponding stock prices.',
     author='Michael Browne',
     license='',
 )
